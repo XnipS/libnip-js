58728577e226f02c8a01a998bba273f121855e17
XnipS <realxnips@gmail.com>
2022-12-23 09:57:17 +1100

    Added package infomation

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # libnip-js
+
+[![License](https://img.shields.io/github/license/xnips/libnip-js)](LICENSE)
+
 Small custom library for npm projects.
+
+# Contents
+
+### LogToFile: function (prompt, file)
+	Logs an input `prompt (string)` to path `file (string)`
+### ArrayContains: function (arr, value, row)
+	Checks if 2D array `arr (array)` contains a value `value (*)` in row `row (int)`
+### RelativeTimeReply: function (totalSeconds) or (days, hours, minutes, seconds)
+	Returns a readable description of a selected time and date `totalSeconds (long)`
+
+
+# Usage
+
+Import into your project, put in includes, use as normal functions.
---
+++ index.js
@@ -0,0 +1,59 @@
+// Includes
+const fs = require("fs");
+
+// Export functions
+module.exports = {
+
+	//Log to file
+	LogToFile: function (prompt, file) {
+		fs.writeFileSync(
+			file,
+			prompt,
+			(err) => {
+				if (err) {
+					throw err;
+				}
+			}
+		);
+	},
+
+	// Custom method to test for value
+	ArrayContains: function (arr, value, row) {
+		var output = false;
+		for (let i = 0; i < arr.length; i++) {
+			// Get the size of the inner array
+			if (arr[i][row] == value) {
+				output = true;
+			}
+		}
+		return output;
+	},
+
+	// Seconds to other
+	RelativeTimeReply: function (totalSeconds) {
+		let days = Math.floor(totalSeconds / 86400);
+		totalSeconds %= 86400;
+		let hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = Math.floor(totalSeconds % 60);
+		return RelativeTimeReply(days, hours, minutes, seconds);
+	},
+
+	// Reply using relative time
+	RelativeTimeReply: function (days, hours, minutes, seconds) {
+		let uptime = "Unknown error occurred...";
+		if (days > 0) {
+			uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+		}
+		else if (hours > 0) {
+			uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+		}
+		else if (minutes > 0) {
+			uptime = `${minutes} minutes and ${seconds} seconds`;
+		} else {
+			uptime = `${seconds} seconds`;
+		}
+		return uptime;
+	}
+}
